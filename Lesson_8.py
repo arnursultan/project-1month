3ff6e824e60757b42add95975020467be7804735
arnursultan <badasspubgm@gmail.com>
2022-12-30 20:04:28 +0600

    your commit

--- Lesson_8.py
+++ Lesson_8.py
@@ -73,35 +73,37 @@
 # with open('geek.txt', 'r') as r:
 #     print(r.read())
 
-import random
-import time
-
-# print(random.randint(1, 100))
-
-def generate_random_number():
-    return random.randint(1, 10)
-
-def user_contacts(name:str, number:str, secret_key:str):
-    with open('wins.txt', 'a+', encoding='utf-8') as win:
-        win.write(f"Имя: {name}, номер: {number}, ключ: {secret_key}, дата: {time.ctime()}\n")
+# import random
+# import time
 #
-def main():
-    random_number = generate_random_number()
-    n = 0
-    while True:
-        user_input = int(input("Введите число: "))
-        n += 1
-        if random_number == user_input:
-            print("Вы выиграли! Введите свои данные для контакта")
-            name = input("Введите свое имя: ")
-            number = input("Введите свой номер: ")
-            secret_key = input("Введите секретное слово для получения приза:")
-            user_contacts(name, number, secret_key)
-            print("Спасибо, ваши данные записаны")
-            break
+# # print(random.randint(1, 100))
+#
+# def generate_random_number():
+#     return random.randint(1, 10)
+#
+# def user_contacts(name:str, number:str, secret_key:str):
+#     with open('wins.txt', 'a+', encoding='utf-8') as win:
+#         win.write(f"Имя: {name}, номер: {number}, ключ: {secret_key}, дата: {time.ctime()}\n")
+# #
+# def main():
+#     random_number = generate_random_number()
+#     n = 0
+#     while True:
+#         user_input = int(input("Введите число: "))
+#         n += 1
+#         if random_number == user_input:
+#             print("Вы выиграли! Введите свои данные для контакта")
+#             name = input("Введите свое имя: ")
+#             number = input("Введите свой номер: ")
+#             secret_key = input("Введите секретное слово для получения приза:")
+#             user_contacts(name, number, secret_key)
+#             print("Спасибо, ваши данные записаны")
+#             break
+#
+#         else:
+#             print(f"Вы не угадали, бездарь {5 - n} попыток")
+#             if 5 - n == 0:
+#                 break
+# main()
 
-        else:
-            print(f"Вы не угадали, бездарь {5 - n} попыток")
-            if 5 - n == 0:
-                break
-main()
+print("GeekStudio")
