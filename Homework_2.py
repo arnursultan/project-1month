0f470c9b1bf2f66beed13b429a16dfd9ea8b6a26
Nursultan Zhanybekov <121675952+arnursultan@users.noreply.github.com>
2023-06-05 18:06:40 +0600

    Update Homework_2.py

--- Homework_2.py
+++ Homework_2.py
@@ -1,4 +1,3 @@
-#Задание 1
 # login = "Kann"
 # password = "Key"
 # user_login = input("Login: ")
@@ -10,10 +9,46 @@
 #     print("""Error!
 #     Password is incorrect. Please, try again.""")
 
+# login = "Kann"
+# password = "Key"
+# user_login = input("Login: ")
+# user_password = input("Password: ")
+# if user_password !=password and user_login != login:
+#     print("Password is incorrect. Please, try again")
+# elif len(user_password) <= 2:
+#     print("Password is incorrect. Please, try again")
+#
+# elif "123" in  user_password:
+#     print("Password is incorrect. Please, try again")
+
+# else:
+#     print("Password is correct. You are welcome")
+
 #Задание 2
-int("Погода")
-print(int)
+# weather  = int(input(" Напишите погоду за окном: "))
+# if weather <= -30 :
+#     print("Там так холодно, лучше дома сиди!")
+# elif weather > -30 and weather <0:
+#     print("“Холодновато. Оденься потеплее!”")
+# elif weather>=0 and weather<15:
+#     print("Прохладно. Куртку накинь!")
+# elif weather >=15 and weather <30:
+#     print("Тепло. Иди погуляй в парке!")
+# elif weather>=30 and weather <=50:
+#     print("Ого, как жарко!")
+# elif weather>50:
+#     print("Там такая жара, лучше сиди дома!")
+# else:
+#     print("Ошибка!")
 
 #Задание 3
+# a = ("Advertising companies say advertising is necessary and important. It informs people about new products. Advertising hoardings in the street make our environment colourful. And adverts on TV are often funny. Sometimes they are mini-dramas and we wait for the next programme in the mini-drama. Advertising can educate, too. Adverts tell us about new, healthy products. And adverts in magazines give us ideas for how to look prettier, be fashionable and be successful. Without advertising, life is boring and colourless. But some consumers argue that advertising is a bad thing. They say that advertising is bad for children. Adverts make children ‘pester’ their parents buy things for them. Advertisers know we love our children and want to give them everything. So they use children’s ‘pester power’ to sell their products. Finally, con  sumers say, if there is advertising there must be rules. Somadverts advertise unhealthy things like cigarettes and make people waste their money.")
+# print(" Букв << s >> : ", a.count("s"))
+# print(" Букв << t >> : ", a.count("t"))
+# print(" Букв << S >> : ", a.count("S"))
+# print(" Букв << T >> : ", a.count("T"))
 
 #Дополнительное задание
+# it = "AidanaAdilet"
+#      #01234567891011
+# print(it[0] + it[6] + it[1] + it[2] + it[7] + it[8] + it[3] + it[9] + it[4] + it[10] + it[5])
